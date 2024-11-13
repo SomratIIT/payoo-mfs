808d42f4ac1d35e598e0bcdd704cb7844eca18e3
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-11-13 18:42:47 +0600

    Got the Amount and Pin NUmber

--- js/home.js
+++ js/home.js
@@ -1,8 +1,16 @@
 
-// Setp 01 --add an event handler to the add money btn inside the form
+// Step 01 --add an event handler to the add money btn inside the form
 document.getElementById('btn-add-money').addEventListener('click',function(event)
 {
     event.preventDefault();
 
+// Step 2 --get money
+const addMoneyInput = document.getElementById('input-add-money').value;
+console.log(addMoneyInput);
 
+
+// Step 3--- get the pin number
+
+const addPinInput= document.getElementById('input-pin-number').value;
+console.log(addPinInput);
 })
